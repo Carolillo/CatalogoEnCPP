8aa6074a4a70dec03bac9a3ef471fcf6bfdf5303
Neck666 <f33ld4b34t@gmail.com>
2015-06-21 13:17:43 +0200

    Añadidos headers a ListaPacientes.h, pendiente comprobar cuales son o no necesarios.

--- ClinicURJC/ListaPacientes.h
+++ ClinicURJC/ListaPacientes.h
@@ -1,42 +1,57 @@
 /*
- * Paciente.h
+ * ListaPacientes.h
  *
  *  Created on: 20-jun-2015
  *      Author: Neck
  */
 
-#ifndef ClinicURJC_Paciente_H
-#define ClinicURJC_Paciente_H
-#include <string.h>
+#ifndef ClinicURJC_ListaPacientes_H
+#define ClinicURJC_ListaPacientes_H
 
-class Paciente: public Persona
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+#include "NodoPaciente.h"
+#include "NodoPaciente.cpp"
+
+using namespace std;
+
+
+template <class Paciente>
+
+class ListaPacientes
 {
 
-	public :
-		/*Constructor*/
-		Paciente();
-		
-		/*Setters y Getters*/
-		void setNombre (std::string);
-		void setEdad (int);
-		void addServicio (std::string, Medico);
-		
-		std::string getNombre ();
-		int getEdad ();
-		
-				
-		
-	private :
-	
-		/*Atributos*/
-		
-		
-//		std::string nombre;
-//		int edad;
+	public:
+    	ListaPacientes();
+    	~ListaPacientes();
+
+    	void add_head(Paciente);
+    	void add_end(Paciente);
+    	void add_sort(Paciente);
+    	void concat(ListaPacientes);
+    	void del_all();
+    	void del_by_data(Paciente);
+    	void del_by_position(int);
+    	void fill_by_user(int);
+    	void fill_random(int);
+    	void intersection(ListaPacientes);
+    	void invert();
+    	void load_file(string);
+    	void print();
+    	void save_file(string);
+    	void search(Paciente);
+    	void sort();
+
+private:
+    NodoPaciente<Paciente> *m_head;
+    int m_num_nodes;
 
 
 };
 
 
 
-#endif /* PERSONA_H_ */
+#endif /* ClinicURJC_ListaPacientes_H */
